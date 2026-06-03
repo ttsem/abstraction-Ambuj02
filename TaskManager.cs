9535c60463c0124dd5d7ffe625f41e1384af78b8
Ambuj02 <80207595+Ambuj02@users.noreply.github.com>
2026-06-03 10:43:25 +0530

    initial commit

--- TaskManager.cs
+++ TaskManager.cs
@@ -1,6 +1,16 @@
 using System;
 
-public class Printer
+public interface IPrinter
+{
+    void Print();
+}
+
+public interface IScanner
+{
+    void Scan();
+}
+
+public class Printer : IPrinter
 {
     public void Print()
     {
@@ -8,7 +18,7 @@ public class Printer
     }
 }
 
-public class Scanner
+public class Scanner : IScanner
 {
     public void Scan()
     {
@@ -16,16 +26,28 @@ public class Scanner
     }
 }
 
+public class PrintScanner : IPrinter, IScanner
+{
+    public void Print()
+    {
+        Console.WriteLine("PrintScanner: Printing document...");
+    }
+
+    public void Scan()
+    {
+        Console.WriteLine("PrintScanner: Scanning document...");
+    }
+}
+
 public class TaskManager
 {
-  
-    public void PrintTask(int taskId, Printer printer)
+    public void PrintTask(int taskId, IPrinter printer)
     {
         Console.WriteLine($"Executing Print Task: {taskId}");
         printer.Print();
     }
 
-    public void ScanTask(int taskId, Scanner scanner)
+    public void ScanTask(int taskId, IScanner scanner)
     {
         Console.WriteLine($"Executing Scan Task: {taskId}");
         scanner.Scan();
@@ -38,10 +60,14 @@ public class Program
     {
         var printer = new Printer();
         var scanner = new Scanner();
+        var printScanner = new PrintScanner();
 
         var scheduler = new TaskManager();
 
         scheduler.PrintTask(101, printer);
         scheduler.ScanTask(102, scanner);
+
+        scheduler.PrintTask(103, printScanner);
+        scheduler.ScanTask(104, printScanner);
     }
 }
